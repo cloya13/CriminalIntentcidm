6ec920d85f70664a72bda4d1198c488717fe3f44
Christian Loya <caloya1@buffs.wtamu.edu>
2018-03-19 12:54:03 -0500

    ch 8 commit

--- app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -1,14 +1,19 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class CrimeActivity extends AppCompatActivity {
+/**
+ * Created by Christian Loya on 3/19/2018.
+ */
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
 
@@ -16,11 +21,8 @@ public class CrimeActivity extends AppCompatActivity {
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
         if (fragment == null) {
-            fragment = new CrimeFragment();
-            fm.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit();
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
         }
     }
 }
-//
